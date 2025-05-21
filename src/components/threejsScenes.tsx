4e97b5a6152ba9f70f889a0b6eddc804d74c71d2
Vinay Mahajan <mahajan.vinay10@gmail.com>
2025-05-22 01:43:41 +0530

    Add Three.js integration and update dependencies

---
+++ src/components/threejsScenes.tsx
@@ -0,0 +1,77 @@
+'use client'
+import { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const ThreeScene = () => {
+  const mountRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xdddddd)
+
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+    camera.position.set(2, 2, 5)
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.shadowMap.enabled = true
+    mountRef.current?.appendChild(renderer.domElement)
+
+    const controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
+
+    const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+    const cubeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
+    const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+    cube.castShadow = true
+    cube.position.y = 2
+    scene.add(cube)
+
+    const groundGeometry = new THREE.PlaneGeometry(10, 10)
+    const groundMaterial = new THREE.MeshStandardMaterial({ color: 0xff5555 })
+    const ground = new THREE.Mesh(groundGeometry, groundMaterial)
+    ground.rotation.x = -Math.PI / 2
+    ground.position.y = 0
+    ground.receiveShadow = true
+    scene.add(ground)
+
+
+    const light = new THREE.DirectionalLight(0xffffff, 1)
+    light.position.set(3, 5,2)
+    light.castShadow = true
+    scene.add(light)
+    scene.add(new THREE.AmbientLight(0xffffff, 0.3))
+
+    const animate = () => {
+      requestAnimationFrame(animate)
+      cube.rotation.y += 0.01
+      controls.update()
+      renderer.render(scene, camera)
+    }
+
+    animate()
+
+    const handleResize = () => {
+      camera.aspect = window.innerWidth / window.innerHeight
+      camera.updateProjectionMatrix()
+      renderer.setSize(window.innerWidth, window.innerHeight)
+    }
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      renderer.dispose()
+      mountRef.current?.removeChild(renderer.domElement)
+    }
+  }, [])
+
+  return <div ref={mountRef} style={{ width: '100vw', height: '100vh', margin: 0 }} />
+}
+
+export default ThreeScene
